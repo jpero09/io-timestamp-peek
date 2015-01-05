757e2e244f8149a99bb39ce0d00325e29a103979
Jason Pero <jpero@knotice.com>
2015-01-05 17:00:38 -0500

    Working samples. bit of documenation

--- src/server/js/ng/controllers.js
+++ src/server/js/ng/controllers.js
@@ -12,7 +12,9 @@
     vm.sampleData= [
       Date.now(),
       new Date(2015,0,1),
-      new Date(1999, 11, 31)
+      new Date(1999, 11, 31),
+      '2014-04-25T01:32:21.196+0600',
+      'bad input data'
     ];
   }
 })();
--- src/server/js/ng/directives.js
+++ src/server/js/ng/directives.js
@@ -9,16 +9,21 @@
   function ioTimeStampPeek() {
     return {
       restrict: 'E',
-      scope: { value: '=' },
+      scope: { value: '=', invalidlabel:'@' },
       replace: true,
       template: '<div class="timeStampPeek"><div class="flipper"><div class="momentValue">{{mValue}}</div><div class="rawValue">{{rValue}}</div></div></div>',
       link: linkFn
     };
 
-    function linkFn(scope, element, attrs){
+    function linkFn(scope){
       scope.$watch('value', function onDateChange(newVal){
-        if(!newVal) return;
-        var cleanVal = moment(newVal);
+        var cleanVal = moment(new Date(newVal));
+        if(!cleanVal.isValid()){
+          var err = scope.invalidlabel ? scope.invalidlabel : moment.localeData().invalidDate();
+          scope.rValue = err;
+          scope.mValue = err;
+          return;
+        }
         scope.rValue = cleanVal.format("MM/DD/YYYY, h:mm:ss a");
         scope.mValue = cleanVal.fromNow();
       });
