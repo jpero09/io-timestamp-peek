2bef087b3f1866ce8bb0f51680dbe2caeee7c90f
jpero <jpero@knotice.com>
2015-01-06 12:31:32 -0500

    Few extra attributes. Moved styl to its own file.

--- src/server/js/ng/directives.js
+++ src/server/js/ng/directives.js
@@ -9,23 +9,37 @@
   function ioTimeStampPeek() {
     return {
       restrict: 'E',
-      scope: { value: '=', invalidlabel:'@' },
+      scope: {
+        value: '=',
+        invalidLabel: '@',
+        defaultView: '@',
+        format: '@'
+      },
       replace: true,
-      template: '<div class="timeStampPeek"><div class="flipper"><div class="momentValue">{{mValue}}</div><div class="rawValue">{{rValue}}</div></div></div>',
+      template: '<div class="timeStampPeek"><div class="flipper"><div>{{mValue}}</div><div>{{rValue}}</div></div></div>',
       link: linkFn
     };
 
     function linkFn(scope){
+      var EXACT_VIEW = "exact";
+      var DEFAULT_FORMAT = "MM/DD/YYYY, h:mm:ss a";
       scope.$watch('value', function onDateChange(newVal){
         var cleanVal = moment(new Date(newVal));
         if(!cleanVal.isValid()){
-          var err = scope.invalidlabel ? scope.invalidlabel : moment.localeData().invalidDate();
+          var err = scope.invalidLabel ? scope.invalidLabel : moment.localeData().invalidDate();
           scope.rValue = err;
           scope.mValue = err;
           return;
         }
-        scope.rValue = cleanVal.format("MM/DD/YYYY, h:mm:ss a");
-        scope.mValue = cleanVal.fromNow();
+        var format = (scope.format) ? scope.format : DEFAULT_FORMAT;
+        if(scope.defaultView && scope.defaultView.toLowerCase() === EXACT_VIEW){
+          scope.mValue = cleanVal.format(format);
+          scope.rValue = cleanVal.fromNow();
+        }
+        else{
+          scope.rValue = cleanVal.format(format);
+          scope.mValue = cleanVal.fromNow();
+        }
       });
     }
   }
